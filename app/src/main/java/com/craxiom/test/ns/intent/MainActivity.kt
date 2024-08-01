286360cfe1985a2ee09dcf76dfeaa09b2d4ef28c
Christian Rowlands <christian.rowlands@gmail.com>
2024-08-01 14:10:19 -0400

    Adds a button for starting the NS activity

--- app/src/main/java/com/craxiom/test/ns/intent/MainActivity.kt
+++ app/src/main/java/com/craxiom/test/ns/intent/MainActivity.kt
@@ -66,7 +66,10 @@ class MainActivity : ComponentActivity() {
                             )*/
 
                             startNetworkSurveyIntent.addFlags(Intent.FLAG_INCLUDE_STOPPED_PACKAGES)
-                            startNetworkSurveyIntent.component = ComponentName("com.craxiom.networksurvey", "com.craxiom.networksurvey.services.NetworkSurveyService")
+                            startNetworkSurveyIntent.component = ComponentName(
+                                "com.craxiom.networksurvey",
+                                "com.craxiom.networksurvey.services.NetworkSurveyService"
+                            )
                             startForegroundService(startNetworkSurveyIntent)
 
                             Log.i("MainActivity", "Sent the start survey intent")
@@ -80,12 +83,51 @@ class MainActivity : ComponentActivity() {
                             val stopNetworkSurveyIntent =
                                 Intent("com.craxiom.networksurvey.STOP_SURVEY")
 
-                            stopNetworkSurveyIntent.component = ComponentName("com.craxiom.networksurvey", "com.craxiom.networksurvey.services.NetworkSurveyService")
+                            stopNetworkSurveyIntent.component = ComponentName(
+                                "com.craxiom.networksurvey",
+                                "com.craxiom.networksurvey.services.NetworkSurveyService"
+                            )
                             stopService(stopNetworkSurveyIntent)
 
                             Log.i("MainActivity", "Sent the stop survey intent")
                         }) {
                             Text("Send Stop Intent")
+                        }
+
+                        Spacer(modifier = Modifier.height(24.dp))
+
+                        Button(onClick = {
+                            val startNetworkSurveyIntent =
+                                Intent("com.craxiom.networksurvey.START_SURVEY")
+
+                            // These flags are for file logging
+                            startNetworkSurveyIntent.putExtra("cellular_file_logging", true)
+                            startNetworkSurveyIntent.putExtra("wifi_file_logging", true)
+
+                            // MQTT is configured via a JSON string
+                            val mqttConfigJsonString = "{\"mqtt_username\": \"auser\", " +
+                                    "\"mqtt_password\": \"apassword\", " +
+                                    "\"mqtt_host\": \"cloud.mymqttserver.com\", " +
+                                    "\"mqtt_port\": 8883, " +
+                                    "\"mqtt_client\": \"aclient\", " +
+                                    "\"mqtt_tls\": true, " +
+                                    "\"cellular_stream_enabled\": true, " +
+                                    "\"wifi_stream_enabled\": true"
+                            /*startNetworkSurveyIntent.putExtra(
+                                "mqtt_config_json",
+                                mqttConfigJsonString
+                            )*/
+
+                            startNetworkSurveyIntent.addFlags(Intent.FLAG_INCLUDE_STOPPED_PACKAGES)
+                            startNetworkSurveyIntent.component = ComponentName(
+                                "com.craxiom.networksurvey",
+                                "com.craxiom.networksurvey.NetworkSurveyActivity"
+                            )
+                            startActivity(startNetworkSurveyIntent)
+
+                            Log.i("MainActivity", "Sent the start survey intent")
+                        }) {
+                            Text("Send Start Activity Intent")
                         }
                     }
                 }
